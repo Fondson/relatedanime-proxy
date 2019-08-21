531612ee42a5475c256c845f4120acc9fd767ab9
Fondson Tran <fondsont@gmail.com>
2019-08-21 19:07:05 -0400

    add rate limitng

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var proxy = require('express-http-proxy');
 var pingSelf = require('./pingSelf');
+var rateLimit = require('function-rate-limit');
+
+//                         reqs/ms
+const PROXY_FN = rateLimit(1, 5000, proxy('www.myanimelist.net'));
 
 pingSelf.ping();
 var app = express();
@@ -14,8 +18,8 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent');
     next();
 });
- 
-app.use('/proxy', proxy('www.myanimelist.net'));
+
+app.use('/proxy', PROXY_FN);
 
 app.listen(app.get('port'), () => {
     console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
